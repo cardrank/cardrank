92f1edf6a317797dbe960c40c6507ccc01fb79de
cardrank <cardrank@protonmail.com>
2023-02-17 16:17:56 +0700

    fixing dealer example

--- _example/dealer/main.go
+++ _example/dealer/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"math/rand"
-	"strconv"
 	"time"
 
 	"github.com/cardrank/cardrank"
@@ -16,65 +15,58 @@ func main() {
 		// note: use a better pseudo-random number generator
 		r := rand.New(rand.NewSource(seed))
 		fmt.Printf("------ %s %d ------\n", typ, seed)
-		// setup dealer
+		// setup and display dealer information
 		d := typ.Dealer(r, 3, players)
+		desc := typ.Desc()
+		fmt.Printf("Eval: %s\n", desc.Eval)
+		fmt.Printf("HiComp: %s LoComp: %s\n", desc.HiComp, desc.LoComp)
+		fmt.Printf("HiDesc: %s LoDesc: %s\n", desc.HiDesc, desc.LoDesc)
 		// display cards in deck
-		all := d.All()
-		fmt.Printf("Deck (%s, %d):\n", typ.DeckType(), len(all))
-		for i := 0; i < len(all); i += 8 {
-			fmt.Printf("  %v\n", all[i:min(i+8, len(all))])
+		deck := d.Deck.All()
+		fmt.Printf("Deck: %s [%d]\n", desc.Deck, len(deck))
+		for i := 0; i < len(deck); i += 8 {
+			fmt.Printf("  %v\n", deck[i:min(i+8, len(deck))])
 		}
-		// need to know if type supports double or supports lows as it
-		// determines the output display below
-		double, low := typ.Double(), typ.Low()
 		for d.Next() {
-			fmt.Printf("%s:\n", d)
-			// collect pockets and hi board
-			pockets, hiBoard = d.Deal(pockets, hiBoard, players)
-			// display pockets
-			if 0 < d.Pocket() {
+			fmt.Printf("%s\n", d)
+			if d.HasPocket() {
 				for i := 0; i < players; i++ {
-					fmt.Printf("  % 2d: %v\n", i, pockets[i])
+					fmt.Printf("  %d: %v\n", i, d.Pockets[i])
 				}
 			}
-			if 0 < d.Board() {
-				// display hiboard
-				fmt.Printf("  Board: %v\n", hiBoard)
-				if double {
-					// collect and display lo board, if any
-					loBoard = d.DealBoard(loBoard, false)
-					fmt.Printf("         %v\n", loBoard)
+			if v := d.Discarded(); len(v) != 0 {
+				fmt.Printf("  Discard: %v\n", v)
+			}
+			if d.HasBoard() {
+				for i := 0; i < len(d.Boards); i++ {
+					fmt.Printf("  Run %d: %v\n", i, d.Boards[i].Hi)
+					if d.Double {
+						fmt.Printf("         %v\n", d.Boards[i].Lo)
+					}
 				}
 			}
-		}
-		// rank the hands
-		hi := typ.RankHands(pockets, hiBoard)
-		var lo []*cardrank.Hand
-		if double {
-			lo = typ.RankHands(pockets, loBoard)
 		}
 		fmt.Printf("Showdown:\n")
-		for i := 0; i < len(hi); i++ {
-			// display hi hand eval
-			fmt.Printf(" % 2d: %04d %v %v %s\n", i, hi[i].HiRank, hi[i].HiBest, hi[i].HiUnused, hi[i].Description())
-			// display lo hand eval
-			switch {
-			case double:
-				fmt.Printf("     %04d %v %v %s\n", lo[i].HiRank, lo[i].HiBest, lo[i].HiUnused, lo[i].Description())
-			case low:
-				fmt.Printf("     %04d %v %v %s\n", hi[i].LoRank, hi[i].LoBest, hi[i].LoUnused, hi[i].LowDescription())
+		for d.NextResult() {
+			run, res := d.Result()
+			fmt.Printf("  Run %d:\n", run)
+			for i := 0; i < players; i++ {
+				if d.Active[i] {
+					hi := res.Evals[i].HiDesc()
+					fmt.Printf("    %d: %v %v %s\n", i, hi.Best, hi.Unused, hi)
+					if d.Low || d.Double {
+						lo := res.Evals[i].LoDesc()
+						fmt.Printf("       %v %v %s\n", lo.Best, lo.Unused, lo)
+					}
+				} else {
+					fmt.Printf("    %d: inactive\n", i)
+				}
 			}
-		}
-		fmt.Printf("Result:\n")
-		// display winner(s)
-		win := cardrank.NewWin(hi, lo, low)
-		fmt.Printf("  %s\n", win.HiDesc(func(_, i int) string {
-			return strconv.Itoa(i)
-		}))
-		if !win.Scoop() && (double || low) {
-			fmt.Printf("  %s\n", win.LoDesc(func(_, i int) string {
-				return strconv.Itoa(i)
-			}))
+			hi, lo := res.Win()
+			fmt.Printf("    Result: %s with %S\n", hi, hi)
+			if lo != nil {
+				fmt.Printf("            %s with %S\n", lo, lo)
+			}
 		}
 	}
 }
